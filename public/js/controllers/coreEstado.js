dc7d46b5f095aef4cdf06ee1f6ae6606243331a3
Daniel Salamanca <contacto@danielsalamanca.com>
2016-05-15 02:07:58 -0500

    gráficas totalmente corregidas, solo falta las alertas

--- public/js/controllers/coreEstado.js
+++ public/js/controllers/coreEstado.js
@@ -6,7 +6,7 @@ my_angular_app.service("coneccion", function () {
     //tiene el uid y el metodo
     var callbacksMapa = {};
 
-     socket.on("mapaFiltrado", function (params) {
+    socket.on("mapaFiltrado", function (params) {
         //llamar el callback que tien el guid y le paso como parametro el geojson
         callbacksMapa[params.guid](params.geojson);
         delete callbacksMapa[params.guid];
@@ -70,6 +70,7 @@ my_angular_app.service("coneccion", function () {
 
 my_angular_app.controller("home_controller", ["$scope", "coneccion", function ($scope, coneccion) {
     //console.log($scope);
+    var pintarunavez = 1;
     var socket = io();
     var capaConPuntosCargados;
     var capaConPoligonosCargados, capaConPoligonosCargadosPolicia, capaConPoligonosCargadosArboles, capaConPoligonosCargadosBasuras, capaConPoligonosCargadosAcceso, capaConPoligonosCargadosJuegos, capaConPoligonosCargadosSillas, capaConPoligonosCargadosLuz;
@@ -146,15 +147,62 @@ my_angular_app.controller("home_controller", ["$scope", "coneccion", function ($
         pesoGlobalJuegos = 10,
         pesoGlobalSillas = 10,
         pesoGlobalLuz = 10;
-//VARIABLES PARA CREAR GRAFICO DE COORDENADAS PARAELAS
-    var nombresManzanas = ["m1","m2","m3","m4","m5","m6","m7","m8","m9","m10","m11","m12","m13","m14","m15","m16","m17","m18","m19","m20","m21","m22","m23","m24","m25","m26","m27","m28","m29","m30","m31","m32","m33","m34","m35","m36","m37","m38","m39","m40","m41","m42","m43","m44","m45","m46","m47","m48"]
-     //var m1=[1, -0, 0, 0, 0, 3, 7, "m1"], m2=[1, -1, 1, 2, 1, 6, 1, "m2"],m3=[2, -2, 4, 4, 0.5, 2, 7, "m3"],m4=[3, -3, 9, 6, 0.33, 4, 5, "m4"], m5=[4, -4, 16, 8, 0.25, 9, 3, "m5"], m6=[5, -7, 16, 8, 0.25, 9, 5, "m6"], m7=[6, -3, 16, 8, 0.25, 9, 1, "m7"],m8=[7, -0, 0, 5, 0, 3, 6, "m8"],m9=[],m10=[],m11=[],m12=[],m13=[],m14=[],m15=[],m16=[],m17=[],m18=[],m19=[],m20=[],m21=[],m22=[],m23=[],m24=[],m25=[],m26=[],m27=[],m28=[],m29=[],m30=[],m31=[],m32=[],m33=[],m34=[],m35=[],m36=[],m37=[],m38=[],m39=[],m40=[],m41=[],m42=[],m43=[],m44=[],m45=[],m46=[],m47=[],m48=[];
-    var m1=[],m2=[],m3=[],m4=[],m5=[],m6=[],m7=[],m8=[],m9=[],m10=[],m11=[],m12=[],m13=[],m14=[],m15=[],m16=[],m17=[],m18=[],m19=[],m20=[],m21=[],m22=[],m23=[],m24=[],m25=[],m26=[],m27=[],m28=[],m29=[],m30=[],m31=[],m32=[],m33=[],m34=[],m35=[],m36=[],m37=[],m38=[],m39=[],m40=[],m41=[],m42=[],m43=[],m44=[],m45=[],m46=[],m47=[],m48=[];
+    //VARIABLES PARA CREAR GRAFICO DE COORDENADAS PARAELAS
+    var nombresManzanas = ["m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9", "m10", "m11", "m12", "m13", "m14", "m15", "m16", "m17", "m18", "m19", "m20", "m21", "m22", "m23", "m24", "m25", "m26", "m27", "m28", "m29", "m30", "m31", "m32", "m33", "m34", "m35", "m36", "m37", "m38", "m39", "m40", "m41", "m42", "m43", "m44", "m45", "m46", "m47", "m48"]
+        //var m1=[1, -0, 0, 0, 0, 3, 7, "m1"], m2=[1, -1, 1, 2, 1, 6, 1, "m2"],m3=[2, -2, 4, 4, 0.5, 2, 7, "m3"],m4=[3, -3, 9, 6, 0.33, 4, 5, "m4"], m5=[4, -4, 16, 8, 0.25, 9, 3, "m5"], m6=[5, -7, 16, 8, 0.25, 9, 5, "m6"], m7=[6, -3, 16, 8, 0.25, 9, 1, "m7"],m8=[7, -0, 0, 5, 0, 3, 6, "m8"],m9=[],m10=[],m11=[],m12=[],m13=[],m14=[],m15=[],m16=[],m17=[],m18=[],m19=[],m20=[],m21=[],m22=[],m23=[],m24=[],m25=[],m26=[],m27=[],m28=[],m29=[],m30=[],m31=[],m32=[],m33=[],m34=[],m35=[],m36=[],m37=[],m38=[],m39=[],m40=[],m41=[],m42=[],m43=[],m44=[],m45=[],m46=[],m47=[],m48=[];
+    var m1 = [],
+        m2 = [],
+        m3 = [],
+        m4 = [],
+        m5 = [],
+        m6 = [],
+        m7 = [],
+        m8 = [],
+        m9 = [],
+        m10 = [],
+        m11 = [],
+        m12 = [],
+        m13 = [],
+        m14 = [],
+        m15 = [],
+        m16 = [],
+        m17 = [],
+        m18 = [],
+        m19 = [],
+        m20 = [],
+        m21 = [],
+        m22 = [],
+        m23 = [],
+        m24 = [],
+        m25 = [],
+        m26 = [],
+        m27 = [],
+        m28 = [],
+        m29 = [],
+        m30 = [],
+        m31 = [],
+        m32 = [],
+        m33 = [],
+        m34 = [],
+        m35 = [],
+        m36 = [],
+        m37 = [],
+        m38 = [],
+        m39 = [],
+        m40 = [],
+        m41 = [],
+        m42 = [],
+        m43 = [],
+        m44 = [],
+        m45 = [],
+        m46 = [],
+        m47 = [],
+        m48 = [];
 
-        var dataparalel = [
- m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13,m14,m15,m16,m17,m18,m19,m20,m21,m22,m23,m24,m25,m26,m27,m28,m29,m30,m31,m32,m33,m34,m35,m36,m37,m38,m39,m40,m41,m42,m43,m44,m45,m46,m47,m48,
+    var dataparalel = [
+ m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, m20, m21, m22, m23, m24, m25, m26, m27, m28, m29, m30, m31, m32, m33, m34, m35, m36, m37, m38, m39, m40, m41, m42, m43, m44, m45, m46, m47, m48,
 ];
-var pc;
+    var pc;
 
     $scope.cambiarPesos = function () {
         pesoGlobalPolicia = $scope.pesoPolicia;
@@ -166,27 +214,27 @@ var pc;
         pesoGlobalLuz = $scope.pesoLuz;
     }
 
-    var dibujarPuntos = function (data){
+    var dibujarPuntos = function (data) {
 
-           capaConPuntosCargados = L.geoJson(data, {
+        capaConPuntosCargados = L.geoJson(data, {
 
-                            style: function (feature) {
-                                return {
-                                    stroke: false,
-                                    color:"#fff",
-                                    fillColor: "#0a2438",
-                                    radius: 2,
-                                    fillOpacity:0.5,
-                                };
-                            },
-                            pointToLayer: function (feature, latlng) {
-                                return L.circleMarker(latlng, geojsonMarkerOptions);
-                            },
-                            onEachFeature: function (feature, layer) {
-                                layer.bindPopup(feature.properties.descripcio);
-                            }
-                        });
-                        capaConPuntosCargados.addTo(map);
+            style: function (feature) {
+                return {
+                    stroke: false,
+                    color: "#fff",
+                    fillColor: "#0a2438",
+                    radius: 2,
+                    fillOpacity: 0.5,
+                };
+            },
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, geojsonMarkerOptions);
+            },
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(feature.properties.descripcio);
+            }
+        });
+        capaConPuntosCargados.addTo(map);
 
     }
 
@@ -242,47 +290,94 @@ var pc;
 
     //función para meter en arrays los valores de poligono y su numero de reportes
     var definirTransparencia = function (data) {
-       // RESETEAR LOS ARRAYS PARA LA GRAFICA DE COORDENADAS PARALELAS
-        m1=[];m2=[];m3=[];m4=[];m5=[];m6=[];m7=[];m8=[];m9=[];m10=[];m11=[];m12=[];m13=[];m14=[];m15=[];m16=[];m17=[];m18=[];m19=[];m20=[];m21=[];m22=[];m23=[];m24=[];m25=[];m26=[];m27=[];m28=[];m29=[];m30=[];m31=[];m32=[];m33=[];m34=[];m35=[];m36=[];m37=[];m38=[];m39=[];m40=[];m41=[];m42=[];m43=[];m44=[];m45=[];m46=[];m47=[];m48=[];
+        // RESETEAR LOS ARRAYS PARA LA GRAFICA DE COORDENADAS PARALELAS
+        m1 = [];
+        m2 = [];
+        m3 = [];
+        m4 = [];
+        m5 = [];
+        m6 = [];
+        m7 = [];
+        m8 = [];
+        m9 = [];
+        m10 = [];
+        m11 = [];
+        m12 = [];
+        m13 = [];
+        m14 = [];
+        m15 = [];
+        m16 = [];
+        m17 = [];
+        m18 = [];
+        m19 = [];
+        m20 = [];
+        m21 = [];
+        m22 = [];
+        m23 = [];
+        m24 = [];
+        m25 = [];
+        m26 = [];
+        m27 = [];
+        m28 = [];
+        m29 = [];
+        m30 = [];
+        m31 = [];
+        m32 = [];
+        m33 = [];
+        m34 = [];
+        m35 = [];
+        m36 = [];
+        m37 = [];
+        m38 = [];
+        m39 = [];
+        m40 = [];
+        m41 = [];
+        m42 = [];
+        m43 = [];
+        m44 = [];
+        m45 = [];
+        m46 = [];
+        m47 = [];
+        m48 = [];
         dataparalel = [
- m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13,m14,m15,m16,m17,m18,m19,m20,m21,m22,m23,m24,m25,m26,m27,m28,m29,m30,m31,m32,m33,m34,m35,m36,m37,m38,m39,m40,m41,m42,m43,m44,m45,m46,m47,m48,
+ m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, m20, m21, m22, m23, m24, m25, m26, m27, m28, m29, m30, m31, m32, m33, m34, m35, m36, m37, m38, m39, m40, m41, m42, m43, m44, m45, m46, m47, m48,
 ]
-// LLENAR LOS ARRAYS PARA LA GRAFICA DE COORDENADAS PARALELAS
-    for (var i = 0; i < data.length; i++) {
-        dataparalel[i].push(Number(data[i].policias));
-    }
-    for (var i = 0; i < data.length; i++) {
-        dataparalel[i].push(Number(data[i].arbol));
-    }
+            // LLENAR LOS ARRAYS PARA LA GRAFICA DE COORDENADAS PARALELAS
         for (var i = 0; i < data.length; i++) {
-        dataparalel[i].push(Number(data[i].caneca));
-    }
+            dataparalel[i].push(Number(data[i].policias));
+        }
         for (var i = 0; i < data.length; i++) {
-        dataparalel[i].push(Number(data[i].estacion));
-    }
+            dataparalel[i].push(Number(data[i].arbol));
+        }
         for (var i = 0; i < data.length; i++) {
-        dataparalel[i].push(Number(data[i].juego));
-    }
+            dataparalel[i].push(Number(data[i].caneca));
+        }
         for (var i = 0; i < data.length; i++) {
-        dataparalel[i].push(Number(data[i].silla));
-    }
+            dataparalel[i].push(Number(data[i].estacion));
+        }
         for (var i = 0; i < data.length; i++) {
-        dataparalel[i].push(Number(data[i].luz));
-    }
+            dataparalel[i].push(Number(data[i].juego));
+        }
         for (var i = 0; i < data.length; i++) {
-        dataparalel[i].push(nombresManzanas[i]);
-    }
+            dataparalel[i].push(Number(data[i].silla));
+        }
+        for (var i = 0; i < data.length; i++) {
+            dataparalel[i].push(Number(data[i].luz));
+        }
+        for (var i = 0; i < data.length; i++) {
+            dataparalel[i].push(nombresManzanas[i]);
+        }
         //CREACION DE COORDENADAS PARALELAS
 
-    console.log(dataparalel);
+        console.log(dataparalel);
         //document.getElementById("graficaCoordenadasParalelas").removeChild;
         d3.select("#graficaCoordenadasParalelas").html("");
-       pc = d3.parcoords()("#graficaCoordenadasParalelas")
+        pc = d3.parcoords()("#graficaCoordenadasParalelas")
             .data(dataparalel)
             .render()
             .createAxes()
 
-            .brushMode("1D-axes")
+        .brushMode("1D-axes")
             .color("rgba(0,200,0,0.3)")
             .alphaOnBrushed(0.6)
             .brushedColor("#000");
@@ -298,7 +393,7 @@ var pc;
         var maximoReportesporManzanaPolicia = d3.max(data, function (d) {
             return Number(d.policias);
         });
-        document.getElementById("maxoimoPolicias").innerHTML = String(maximoReportesporManzanaPolicia);
+        //document.getElementById("maxoimoPolicias").innerHTML = String(maximoReportesporManzanaPolicia);
         var maximoReportesporManzanaArboles = d3.max(data, function (d) {
             return Number(d.arbol);
         });
@@ -447,23 +542,31 @@ var pc;
             var opacidad = opacidadesPoligonos[gid];
             l.setStyle({
                 fillColor: "#144a78",
-                color:"#fff",
+                color: "#fff",
                 fillOpacity: opacidad
             });
-            l.bindPopup("<div id='popUpPoligono'><div id='elementosUsados'><div id='tituloPop'><span>Reportes:</span></div>"+
-                "<div><img src='imagenes/iconos/iconoPolicia.png'><span>"+cantidadDePolicias[gid]+"</span></div>"+
-                "<div><img src='imagenes/iconos/iconoArbol.png'><span>"+cantidadDeArboles[gid]+"</span></div>"+
-                "<div><img src='imagenes/iconos/iconoCaneca.png'><span>"+cantidadDeCanecas[gid]+"</span></div>"+
-                "<div><img src='imagenes/iconos/iconoEstacion.png'><span>"+cantidadDeEstaciones[gid]+"</span></div>"+
-                "<div><img src='imagenes/iconos/iconoJuego.png'><span>"+cantidadDeJuegos[gid]+"</span></div>"+
-                "<div><img src='imagenes/iconos/iconoSilla.png'><span>"+cantidadDeSillas[gid]+"</span></div>"+
-                "<div><img src='imagenes/iconos/iconoLuz.png'><span>"+cantidadDeLuces[gid]+"</span></div></div></div>");
-            l.bindLabel(String(l.feature.properties.gid), { noHide: true }).addTo(map);
+            l.bindPopup("<div id='popUpPoligono'><div id='elementosUsados'><div id='tituloPop'><span>Reportes:</span></div>" +
+                "<div><img src='imagenes/iconos/iconoPolicia.png'><span>" + cantidadDePolicias[gid] + "</span></div>" +
+                "<div><img src='imagenes/iconos/iconoArbol.png'><span>" + cantidadDeArboles[gid] + "</span></div>" +
+                "<div><img src='imagenes/iconos/iconoCaneca.png'><span>" + cantidadDeCanecas[gid] + "</span></div>" +
+                "<div><img src='imagenes/iconos/iconoEstacion.png'><span>" + cantidadDeEstaciones[gid] + "</span></div>" +
+                "<div><img src='imagenes/iconos/iconoJuego.png'><span>" + cantidadDeJuegos[gid] + "</span></div>" +
+                "<div><img src='imagenes/iconos/iconoSilla.png'><span>" + cantidadDeSillas[gid] + "</span></div>" +
+                "<div><img src='imagenes/iconos/iconoLuz.png'><span>" + cantidadDeLuces[gid] + "</span></div></div></div>");
+            if (pintarunavez == 1) {
+//                l.bindLabel(String(l.feature.properties.gid), {
+//                    noHide: true
+//                }).addTo(map);
+                label = new L.Label()
+                label.setContent(String(l.feature.properties.gid))
+                label.setLatLng(capaConPoligonosCargados._layers[each].getBounds().getCenter())
+                map.showLabel(label);
 
+            }
 
 
         }
-
+        pintarunavez = 0;
         for (each in capaConPoligonosCargadosPolicia._layers) {
             var l = capaConPoligonosCargadosPolicia._layers[each];
             var gid = l.feature.properties.gid;
@@ -566,7 +669,7 @@ var pc;
 
 
         map.removeLayer(capaConPoligonosCargados);
-         map.removeLayer(capaConPuntosCargados);
+        map.removeLayer(capaConPuntosCargados);
         mapaPolicia.removeLayer(capaConPoligonosCargadosPolicia);
         mapaArboles.removeLayer(capaConPoligonosCargadosArboles);
         mapaBasuras.removeLayer(capaConPoligonosCargadosBasuras);
@@ -579,8 +682,8 @@ var pc;
             callback: dibujarMapa,
         });
         coneccion.llamarFiltroporHora({
-        callback: dibujarPuntos,
-    });
+            callback: dibujarPuntos,
+        });
 
     }
 
@@ -590,7 +693,7 @@ var pc;
         callback: dibujarMapa
     });
 
-     coneccion.llamarFiltroporHora({
+    coneccion.llamarFiltroporHora({
         callback: dibujarPuntos
     });
 
@@ -607,7 +710,7 @@ var pc;
     // carga de tiling de open maps
     var map = L.map('mapEstado').setView([4.607230, -74.0692000], 16); // Posición inical del mapa (lat, long, zoom)
     map.addLayer(new L.TileLayer(osmUrl, {
-        maxZoom: 16,
+        maxZoom: 18,
         opacity: .3,
     }));
 
@@ -620,7 +723,7 @@ var pc;
     var mapaLuz = L.map('mapaLuz').setView([4.607038, -74.068819], 13.5); // Posición inical del mapa (lat, long, zoom)
 
     //configuración general del mapa
-    map._layersMaxZoom = 16;
+    map._layersMaxZoom = 18;
     map._layersMinZoom = 16;
     L.control.scale({
         position: 'bottomleft', // .. donde aparece
@@ -728,5 +831,3 @@ var pc;
 
 
             }]);
-
-
